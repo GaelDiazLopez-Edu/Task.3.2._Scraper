97c550e44aab89e3caf9eec73d3a07260ae07f9c
gaeldiazlopez <71010831+GaelDiazLopez-Edu@users.noreply.github.com>
2026-02-05 17:59:06 +0100

    Refactor comments in venteminutos.py

--- news_aggregator/spiders/venteminutos.py
+++ news_aggregator/spiders/venteminutos.py
@@ -9,7 +9,7 @@ class VenteMinutosSpider(scrapy.Spider):
             title = article.css('h1 a::text, h2 a::text').get()
             if title:
                 yield {
-                    'title': title.strip(), # Limpieza [cite: 133]
+                    'title': title.strip(), # Limpieza
                     'link': article.css('h1 a::attr(href), h2 a::attr(href)').get(),
-                    'source': '20minutos' # Identificador [cite: 134]
+                    'source': '20minutos' # Identificador
                 }
